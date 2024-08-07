8875f249c48c2265b114813e61546725c043c163
Davide Giacomini <giacomini.davide@outlook.com>
2024-08-07 17:20:44 -0500

    Set up language change

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,7 +1,15 @@
+import Event from "sap/ui/base/Event";
 import Controller from "sap/ui/core/mvc/Controller";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
 /**
  * @namespace ui5.walkthrough.controller
  */
 export default class App extends Controller {
-
+    public onChangeLanguage(oEvent: Event, language: string): void {
+        
+        this.getOwnerComponent()?.setModel(new ResourceModel({
+            bundleName: "it.davidegiacomini.i18n.i18n",
+            bundleLocale: language
+        }), "i18n");
+    }
 };
